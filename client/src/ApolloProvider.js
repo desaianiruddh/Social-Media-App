7e465d2f1f68083c8895737d70eb2f38035fd524
desaianiruddh <aniruddh.d@simformsolutions.com>
2022-08-10 17:35:41 +0530

    change base url

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -14,7 +14,7 @@ import store from './store';
 import { persistor } from './store';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000',
+  uri: 'https://dry-eyrie-33647.herokuapp.com/',
   credentials: 'same-origin',
 });
 const authLink = setContext(() => {
